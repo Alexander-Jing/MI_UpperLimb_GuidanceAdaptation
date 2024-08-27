e31054900436f6449c356042573c88b59c2af9cf
Alexander-Jing <18061523086@163.com>
2024-08-27 23:56:43 +0800

    glove combined for experiments, needs further examination, from computer in 720, 2024.8.27

--- Glove_test.m
+++ Glove_test.m
@@ -9,23 +9,44 @@ fopen(GloveControl);
 
 RandomScene = [0,1,2,0,1,2];
 pause(1);
+sendbuf = uint8(1:2);
+sendbuf(1,1) = hex2dec('ff') ;
+sendbuf(1,2) = hex2dec('ff') ;
+
+fwrite(GloveControl, sendbuf);
 
 for i=1:length(RandomScene)
     pause(10);
     disp(['Send message: ', num2str(RandomScene(i))]);
     switch RandomScene(i) 
         case 0
+            % 如果是握拳的话，过一段时间放松下来
             textSend='G1';
-            %pause(0.1);
-            fwrite(GloveControl, textSend);
+            sendbuf(1,1) = hex2dec('bf') ;
+            sendbuf(1,2) = hex2dec('bf') ;
+            fwrite(GloveControl, sendbuf);
+            pause(3);
+            sendbuf(1,1) = hex2dec('ff') ;
+            sendbuf(1,2) = hex2dec('ff') ;
+            fwrite(GloveControl, sendbuf);
         case 1
+            % 放松状态，由于新林代码的问题，这里需要加两个ff
             textSend='G2';
             %pasue(0.1);
-            fwrite(GloveControl, textSend);
+            sendbuf(1,1) = hex2dec('ff') ;
+            sendbuf(1,2) = hex2dec('ff') ;
+            fwrite(GloveControl, sendbuf);
         case 2
+            % 张开状态，同样过一段时间就要放松下来
             textsend='G3';
             %pause(0.1);
-            fwrite(GloveControl, textsend);
+            sendbuf(1,1) = hex2dec('c0') ;
+            sendbuf(1,2) = hex2dec('c0') ;
+            fwrite(GloveControl, sendbuf);
+            pause(3);
+            sendbuf(1,1) = hex2dec('ff') ;
+            sendbuf(1,2) = hex2dec('ff') ;
+            fwrite(GloveControl, sendbuf);
     end
 end
 
